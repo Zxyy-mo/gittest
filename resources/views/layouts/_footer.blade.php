3a1d343b90331e468ae75e3cf0012bdc9b7ab9f4
ZHOU xiaoyu <1016673080@qq.com>
2020-04-11 11:17:22 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<a class="footer">
+    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1
+ <a href="https://laravel-china.org/courses" target=_blank>12312</a>
+    刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>
