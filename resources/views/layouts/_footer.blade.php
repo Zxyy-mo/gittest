c7795345c602e04f651ecb8d8e49a7e0c8fd834a
ZHOU xiaoyu <1016673080@qq.com>
2020-04-12 00:56:03 +0800

    用户显示页面

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,6 @@
+<footer>
 <a class="footer">
-    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1
+    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1">
  <a href="https://laravel-china.org/courses" target=_blank>12312</a>
     刻意练习，每日精进
     </a>
