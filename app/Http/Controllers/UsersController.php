7265f3403462af38e6c2b24874a52cd56ad7612d
ZHOU xiaoyu <1016673080@qq.com>
2020-04-12 16:54:59 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,16 +12,25 @@ class UsersController extends Controller
     }
     public function show(User $user)
     {
+        //User用户模型接口 $user隐式路由绑定
         return view('users.show', compact('user'));
     }
     public function store(Request $request)
     {
-
+        //验证用户信息
         $this->validate($request,[
             'name'=>'required|max:50',
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+        //调用模型的方式写入数据库
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->name)
+        ]);
+        //session->flash方法只在下一次的请求类中有效
+        session()->flash('success','欢迎,您将在这里有一个新的开始');
+        return redirect()->route('users.show',[$user]);
     }
 }
